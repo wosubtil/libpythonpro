3e9bd14b102c4f8587cc19a88875a01425f2c91a
Welinton <welinton@realizati.com.br>
2020-09-17 09:10:03 -0300

    Instalação e utilização do pytest-mock

--- tests/test_github_api.py
+++ tests/test_github_api.py
@@ -1,12 +1,26 @@
+import pytest
 from unittest.mock import Mock
 
 from libpythonpro import github_api
 
-def test_buscar_avatar():
+@pytest.fixture
+def avatar_url(mocker):
     resp_mock = Mock()
+    url = 'https://avatars0.githubusercontent.com/u/18421824?v=4'
     resp_mock.json.return_value = {'login': 'wosubtil',
                                    'id': 18421824,
-                                   'avatar_url': 'https://avatars0.githubusercontent.com/u/18421824?v=4'}
+                                   'avatar_url': url}
+    get_mock = mocker.patch('libpythonpro.github_api.requests.get')
+    get_mock.return_value = resp_mock
     github_api.requests.get = Mock(return_value=resp_mock)
+    return url
+
+
+def test_buscar_avatar(avatar_url):
     url = github_api.buscar_avatar('wosubtil')
-    assert 'https://avatars0.githubusercontent.com/u/18421824?v=4' == url
+    assert avatar_url == url
+
+
+def test_buscar_avatar_integracao():
+    url = github_api.buscar_avatar('rezon')
+    assert 'https://avatars2.githubusercontent.com/u/527028?v=4' == url
